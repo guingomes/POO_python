c2bdd7c36ac0a2414ea2e368550e44fcb132e6f7
Guilherme Gomes <guingomes@gmail.com>
2021-02-25 14:01:25 -0300

    primeiro programa em python (teste)

--- 2.primeiro_programa.py
+++ 2.primeiro_programa.py
@@ -1,3 +1,3 @@
-#teste feito para primeira publicação no GitHub
+#teste feito para primeira publicação no GitHub com extensão .py
 nome = input('Qual é o seu nome? ')
 print('Prazer em te conhecer {}!'.format(nome))
